6c5574d5a61015c962f3657f84ef8b4efccc2fd1
dilapitan <domlapitan@gmail.com>
2017-08-26 11:27:52 +0800

    Reading of XML files alphabetically

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from selenium.webdriver.common.keys import Keys
 
 import os
 
-chrome_path = "/home/dom/Downloads/chromedriver"
+chromePath = "/home/dom/Downloads/chromedriver"
 
-#browser = webdriver.Chrome(chrome_path)
+#browser = webdriver.Chrome(chromePath)
 #browser.get('http://localhost:8080/')
 
-# reading multiple xml files
-full_path = "/home/dom/Desktop/all-tools"
-test_path = 
-print(full_path)
+# reading multiple xml files in order
+toolsPath = "all-tools" # whole directory of all tools
+toolTypePath = "text-man-xml-files" #specific type of tools
+directory = toolsPath + "/" + toolTypePath
+
+items = os.listdir(directory)
+for path in sorted(items):
+	if path.endswith(".xml"):
+		# insert retrieveValues() here
+		print(path)
+
