7caea0fca48b23d52ce7d4734dd748ee1b747343
dilapitan <domlapitan@gmail.com>
2017-08-26 11:12:19 +0800

    Included the tools folder

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import os
+
 chrome_path = "/home/dom/Downloads/chromedriver"
 
-browser = webdriver.Chrome(chrome_path)
-browser.get('http://localhost:8080/')
+#browser = webdriver.Chrome(chrome_path)
+#browser.get('http://localhost:8080/')
+
+# reading multiple xml files
+full_path = "/home/dom/Desktop/all-tools"
+test_path = 
+print(full_path)
