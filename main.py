0b4759262f5e97b15011248ef155415f8e7baad8
dilapitan <domlapitan@gmail.com>
2017-08-28 09:41:07 +0800

    Created object for automation

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ def retrieveValues(file):
 
 	testParamValues = [] # array of values under <param> of <test>
 	
-	testParams = root.findall('.//test/param')
+	testParams = root.findall('.//test//param')
+	testParamLength = len(testParams) # getting the number of <param> under <test>
+
 	for testParam in testParams:
 		testNameText = testParam.get('name')	# get the value of 'name' attribute
 		testValueText = testParam.get('value')	# get the value of 'value' attribute
@@ -49,10 +51,39 @@ def retrieveValues(file):
 			inputsParamValues.append(inputsNameText)
 			inputsParamValues.append(inputsTypeText)
 		
-	print(testParamValues)
-	print(inputsParamValues)
+	#print(testParamValues)
+	#print(inputsParamValues)
 
+	''' 
+		Object that will be passed for automation 
+	'''
 
+	class Values:
+		def __init__(self, name, datatype, value):
+			self.n = name
+			self.dt = datatype
+			self.v = value
+
+	c = 0
+	paramDouble = testParamLength * 2
+	wholeV = [] # values of all v
+	for i in range(0, testLength):
+		v = [] # values of single <test> 
+		while (c != paramDouble):
+			# object creation
+			testObject = Values(testParamValues[c], testParamValues[c+1], inputsParamValues[c+1])
+			
+			# for checking
+			# print("test name: ", testObject.n)
+			# print("test data type: ", testObject.dt)
+			# print("test value: ", testObject.v)
+			# print("\n")
+			
+			v.append(testObject)
+			c += 2
+		wholeV.append(v)
+
+	return wholeV # values for automation
 
 # reading multiple xml files in order
 toolsPath = "all-tools" # whole directory of all tools
