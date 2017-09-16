70401dcd8461170469ff6f0b9cfac7ca5d0205d8
dilapitan <domlapitan@gmail.com>
2017-09-16 11:16:57 +0800

    Uploading multiple files using Selenium

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 import xml.etree.ElementTree as ET
 import os
 import glob
+import time
 
 # XML parsing and retrieving values
 def retrieveValues(file):
@@ -21,13 +23,14 @@ def retrieveValues(file):
 	
 	testParams = root.findall('.//test//param')
 	
-
 	for testParam in testParams:
 		testNameText = testParam.get('name')	# get the value of 'name' attribute
 		testValueText = testParam.get('value')	# get the value of 'value' attribute
 
 		testParamValues.append(testNameText)
 		testParamValues.append(testValueText)
+
+
 
 	'''
 		Getting the values from <input>
@@ -84,10 +87,11 @@ def retrieveValues(file):
 	#print("retrieved!")
 	return wholeValues, toolName, dataFormat # values for automation
 
-#chromePath = "/home/dom/Downloads/chromedriver"
+chromePath = "/home/dom/Downloads/chromedriver"
 
-#driver = webdriver.Chrome(chromePath)
-#driver.get('http://localhost:8080/')
+driver = webdriver.Chrome(chromePath)
+driver.maximize_window()
+driver.get('http://localhost:8080/')
 
 # reading multiple xml files in order
 toolsPath = "all-tools" # whole directory of all tools
@@ -96,23 +100,80 @@ directory = toolsPath + "/" + toolTypePath
 
 items = glob.glob(directory + "/*.xml")
 items = sorted(items)
+testDataList = []
 for file in items:
 	v, toolName, dformat = retrieveValues(file)
 	
 	# for checking
-	for i in range(0, len(v)):
-		print(i+1)
-		for j in range(0, len(v[i])):
+	for i in range(0, len(v)):			# each test tag
+		
+		'''
+			Uploads ALL the necessary data
+		'''
+
+		getDataLabel = driver.find_element_by_xpath("/html/body/div[1]/div[5]/div[2]/div/div[1]/div[2]/div[1]/a")
+
+		for j in range(0, len(v[i])): 	# each param tag
 			print("\n")
 			print("test name: ", v[i][j].n)
 			print("test data type: ", v[i][j].dt)
 			print("test value: ", v[i][j].v)
+		
+			# Uploading of file given a certain dformat
 
-	# 		''' 
-	# 			Automation / mimicking user
-	# 		'''
+			if (v[i][j].dt not in testDataList): # preventing multiple uploads of same test data
 
-	break
+				testDataDirectory = "/home/dom/Desktop/test-galaxy/test-data/" 
+				testFile = v[i][j].dt
+				testData = testDataDirectory + testFile
+
+				# Get Data	
+				getDataLabel.click()
+
+				# Upload File
+
+				uploadFileLabel = driver.find_element_by_xpath("//*[@id='getext']/div[3]/div/a")
+				uploadFileLabel.click()
+
+				driver.find_element_by_xpath("//*[@id='s2id_autogen1']/a").click()
+				field = driver.find_element_by_xpath("//*[@id='s2id_autogen2_search']")
+				
+				if (dformat == "tabular"):
+					field.send_keys(dformat)
+					field.send_keys(Keys.DOWN)
+					field.send_keys(Keys.ENTER)
+
+				uploadTestData = driver.find_element_by_xpath("//*[@id='regular']/div/div[2]/input")
+				uploadTestData.send_keys(testData)
+
+				testDataList.append(testFile) # preventing multiple uploads of same test data
+
+				startButton = driver.find_element_by_xpath("//*[@id='btn-start']")
+				startButton.click();
+
+				time.sleep(3) # wait for full upload
+
+				closeButton = driver.find_element_by_xpath("//*[@id='btn-close']")
+				closeButton.click();
+
+				time.sleep(7)
+
+				getDataLabel.click() # reclicks Get Data label
+
+		# print(testDataList)
+
+		'''
+			Automation
+		'''
+
+		
+
+
+
+
+	break	
+	
+
 
 
 
